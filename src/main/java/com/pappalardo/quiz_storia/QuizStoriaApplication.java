56859f1a90f7161cdd4aff351568b02f18b36930
cristianpappalardo <cristian.pappalardo@edu-its.it>
2026-09-14 22:26:30 +0200

    Initialized project

---
+++ src/main/java/com/pappalardo/quiz_storia/QuizStoriaApplication.java
@@ -0,0 +1,13 @@
+package com.pappalardo.quiz_storia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizStoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizStoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pappalardo/quiz_storia/QuizStoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pappalardo.quiz_storia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizStoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
